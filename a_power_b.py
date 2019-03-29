dc88293892ed9ad2c8133ea521762732393b1c6e
Munoz Vega <000400505@bga.upb>
2019-03-29 13:54:10 -0500

    respuesta

--- a_power_b.py
+++ a_power_b.py
@@ -2,8 +2,18 @@ def a_power_b(a,b):
     prod=1
     for i in range(0,b):
         prod=prod*a
-    print(prod)
+    return(prod)
 
-a=int(input("ingrese la base"))
-b=int(input("ingrese potencia"))
+
+while True:
+    
+    a=int(input("ingrese el valor de la base"))
+    if a ==0:
+        print("numero es CERO")
+        break
+    b=int(input("ingrese el valor del exponente"))
+    resu=a_power_b(a,b)
+    print("el resultado de su potencia es: ",resu)
+    if a ==0:
+        break
 a_power_b(a,b)
